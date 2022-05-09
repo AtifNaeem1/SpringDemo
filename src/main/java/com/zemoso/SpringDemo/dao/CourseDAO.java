91d311ddb7f0b4bf15e2d1e54bab4f8cc2b1c1f8
AtifNaeem1 <100839831+AtifNaeem1@users.noreply.github.com>
2022-05-09 17:55:56 +0530

    Delete src directory

--- src/main/java/com/zemoso/SpringDemo/dao/CourseDAO.java
+++
@@ -1,9 +0,0 @@
-package com.zemoso.SpringDemo.dao;
-
-
-import com.zemoso.SpringDemo.entity.Course;
-        import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseDAO extends JpaRepository <Course, Integer> {
-    //that's it .. no need to write any code
-}
--- src/main/java/com/zemoso/SpringDemo/dao/StudentDAO.java
+++
@@ -1,9 +0,0 @@
-package com.zemoso.SpringDemo.dao;
-
-import com.zemoso.SpringDemo.entity.Course;
-import com.zemoso.SpringDemo.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentDAO extends JpaRepository<Student, Integer> {
-    //that's it .. no need to write any code
-}
